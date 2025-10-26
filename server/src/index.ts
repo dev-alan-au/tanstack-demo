f9c698b146da2fff49d466ccbf13dae64f8a38ae
Dev Alan <dev.alan.au@gmail.com>
2025-10-26 17:51:38 +1100

    Start prisma db build

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,9 +4,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (_: Request, res: Response) => {
-  res.send("Hello from Node.js running in Docker!");
+	res.send("Hello from Node.js running in Docker!");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+	console.log(`Server running on http://localhost:${PORT}`);
 });
